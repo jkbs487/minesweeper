8702d1cebae53fe4b73d8e101c073c7fd88d6b68
jkbs487 <416578078@qq.com>
2020-08-21 22:55:28 +0800

    feat: 增加从脚本输入棋盘大小的功能

--- start.sh
+++ start.sh
@@ -2,8 +2,16 @@
 
 #set -x
 
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 length width"
+	exit 1
+fi
+
 PROJECT=minesweeper
+LENGTH=$1
+WIDTH=$2
 
 make clean
 make
-./bin/${PROJECT}
+echo "----------------start-------------------"
+./bin/${PROJECT} ${LENGTH} ${WIDTH}
