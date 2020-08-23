a32fe1805ff5644425540d8b4c88d32d7313697e
jkbs487 <416578078@qq.com>
2020-08-23 11:25:59 +0800

    update version

--- start.sh
+++ start.sh
@@ -2,16 +2,9 @@
 
 #set -x
 
-if [ $# -ne 2 ]; then
-	echo "Usage: $0 length width"
-	exit 1
-fi
-
 PROJECT=minesweeper
-LENGTH=$1
-WIDTH=$2
 
 make clean
 make
 echo "----------------start-------------------"
-./bin/${PROJECT} ${LENGTH} ${WIDTH}
+./bin/${PROJECT} 
